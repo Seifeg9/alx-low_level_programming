04919e7f8b1fea27c1f202869f5bbce2bdc54c5b
seifegebriel <seifegebrielhu@gmail.com>
2022-03-14 06:49:09 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 *
 * Return: 0 after printing the function.
 */
-int main (void)
+int main(void)
 {
-puts ("\" programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
