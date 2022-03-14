2e7860899bc7692c5d88f9e94be313db36f3afa2
seifegebriel <seifegebrielhu@gmail.com>
2022-03-14 07:10:39 -0700

    task five printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entery point
+*Hello,World printf task
+*Return: 1 after program exicution
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n";
+return (0);
+}
