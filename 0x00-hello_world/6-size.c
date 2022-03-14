54877ef8c5a7e643e6de630026dbb7db3e211c19
seifegebriel <seifegebrielhu@gmail.com>
2022-03-14 18:32:18 +0300

    new six commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Size of a char: %d bytes(s)\n", sizeof(char));
+    printf("Size of an int: %d bytes(s)\n", sizeof(int));
+    printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+    printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+    printf("Size of a float: %d bytes(s)\n", sizeof(float));
+    return (0);
+}
+
