c7749c1cc11cdcef16d77d2ab91f5df3a6492fae
seifegebriel <seifegebrielhu@gmail.com>
2022-03-20 21:45:01 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: print size
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+int l
+if (size > 0)
+{
+k = size - 1;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j--)
+{
+_putchar (' ');
+}
+for (l = 0; l < size; l++)
+{
+_putchar (35);
+}
+k--;
+_putchar ('\n');
+}
+}
+else
+{
+_putchar ('\n');
+}
+}
+
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: void
+ */
+int main(void)
+{
+int i = 100;
+int j;
+
+j = 1
+while (j <= i)
+{
+if (j % 3 == 0 && j % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (j % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (j % 5 == 0)
+{
+if (j < i)
+printf("Buzz ");
+}
+else
+{
+printf("Buzz");
+}
+else
+{
+printf("%i ", i);
+}
+i++;
+}
+printf("\n");
+return (0);
+}
+
