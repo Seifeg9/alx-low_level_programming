9d09d00a5fe268d48fb599095b6572ee6ab394a0
seifegebriel <seifegebrielhu@gmail.com>
2022-03-20 22:21:15 +0300

    renamed

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+*_isalpha(int c)
+*c: takes in character
+Return: 1 if letter lowercase, uppercase; 0 otherwise
+*/
+int_isalpha(int c)
+{
+if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'[A[B))
+{ 
+return (1);
+}
+else
+{
+return (1);
+}
+}
