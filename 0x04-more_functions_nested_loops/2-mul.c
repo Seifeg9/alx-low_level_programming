edb828af043b845ce4bb46e5f8827f5c5fd4c3a5
seifegebriel <seifegebrielhu@gmail.com>
2022-03-21 21:54:20 +0300

    2-multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,11 @@
 #include <main.h>
+
 /**
-*mul- multiplies two intigers
-*@a: print int 
-*@b: print int
-*Return: Always 0.
+* mul- multiplies two intigers
+* @a: print int 
+* @b: print int
+*
+* Return: Always 0.
 **/
 int mul(int a, int b)
 {
