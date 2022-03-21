99d0e55146ca95718b8a7740507d5f4b91bfd1dd
seifegebriel <seifegebrielhu@gmail.com>
2022-03-21 23:15:07 +0300

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,9 @@
  *
  * Return: Always 0.
  */
-
 void print_line(int n)
 {
-
 int i;
-
 if (n > 0)
 {
 for (i = 0; i < n; i++)
@@ -20,11 +17,9 @@ _putchar (95);
 }
 _putchar ('\n');
 }
-
 else 
 {
 _putchar ('\n');
 }
-
 }
 
