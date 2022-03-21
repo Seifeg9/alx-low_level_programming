92bb2a84ba83fedee7afd6c7982f29886ddecba3
seifegebriel <seifegebrielhu@gmail.com>
2022-03-21 22:53:11 +0300

    5-more_numberss

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,6 +11,7 @@ int i;
 int first_number;
 int second_number;
 int result;
+
 i = 0;
 result = 0;
 while (i < 10)
@@ -21,17 +22,21 @@ if (result < 10)
 {
 second_number = result;
 }
+
 else 
 {
 first_number = result / 10;
 second_number = result % 10;
 _putchar (first_number + '0');
 }
+
 _putchar (second_number + '0');
+
 result++;
 }
 i++;
 result = 0;
 _putchar ('\n');
 }
+
 }
