8854610a8a7257b7572906fe758fcfacc54de5bc
seifegebriel <seifegebrielhu@gmail.com>
2022-03-21 21:46:53 +0300

    upper betty fixx

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*_isupper - checks if c is uppercase character
-*@c: Character to be checked
+* _isupper - checks if c is uppercase character
+* @c: Character to be checked
 *
-*Return: 1 if c is uppercase, 0 otherwise
+* Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
