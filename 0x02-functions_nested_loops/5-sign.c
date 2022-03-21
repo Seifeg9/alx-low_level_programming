57564af51ae8eec10594395b8fe2996c3e64f489
seifegebriel <seifegebrielhu@gmail.com>
2022-03-22 02:00:59 +0300

    5 sign of num

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,29 @@
-#include""[D[D[Cmain.h"
+#include "main.h"
+
 /**
-*print _sign[D[D[D[D[[[C[C[C[C_sign-prints the sign of a number
-@n : inputs an integer[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D*[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-*Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
-*/
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
-_putchar('+");
+_putchar ('+');
 return (1);
 }
+
 else if (n == 0)
 {
-_putchar('0');
+_putchar ('0');
 return (0);
 }
+
 else
-{_putchar('-');
-return(-1);
+{
+_putchar ('-');
+return (-1);
 }
+
 }
