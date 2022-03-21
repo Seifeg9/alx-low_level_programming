618c5afa8c89a5dd8d8678b095f19ee34db9db0f
seifegebriel <seifegebrielhu@gmail.com>
2022-03-21 21:23:13 +0300

    is upper commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
-*_isupper - checks if c is uppercase character.
-*@c: print int
-*Return: 0
+*_isupper - checks if c is uppercase character
+*@c: Character to be checked
+*
+*Return: 1 if c is uppercase, 0 otherwise
 */
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c >= 'A' && c<= 'Z')
 {
