491f8abc902c20186a3868b11b626a9abb90d94b
seifegebriel <seifegebrielhu@gmail.com>
2022-03-25 01:34:43 +0300

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,40 @@
-#include "main.h"
+#include "main.h
 
+/**
+ * separators - checks and ensure that all string is capitalized
+ * @c: character to be checked
+ * Return: if separator return 1. Otherwise return 0;
+ */
+
+int separator(char c)
+{
+switch (c)
+{
+case ' ':
+case '\t':
+case '\n':
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '"':
+case '(':
+case ')':
+case '{':
+case '}':
+return (1);
+
+default:
+return (0);
+}
+
+}
 /**
  * cap_string - capitalizes all words of a string
  * @s: string to uppercase
  * Return: returns the modified string
  */
-
 char *cap_string(char *s)
 {
 int count, upper;
